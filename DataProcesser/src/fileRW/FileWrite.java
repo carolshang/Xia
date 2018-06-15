728e7a6592e5b52367e256eb2dcf48234adb03b1
carolshang <1034324838@qq.com>
2018-06-15 11:14:15 +0800

    data processer project

---
+++ DataProcesser/src/fileRW/FileWrite.java
@@ -0,0 +1,10 @@
+package fileRW;
+
+public class FileWrite {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ DataProcesser/src/processer/MessyData.java
@@ -0,0 +1,21 @@
+package processer;
+
+public class MessyData {
+	//delete the useless field
+	public void DeleteUselessField() {
+		
+	}
+	//fill the data which field of the record is null
+	public void FillBank() {
+		
+	}
+	// discretization
+	public void Discretizaiton() {
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
