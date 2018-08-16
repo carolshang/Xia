d7b4627d8a9adcbf9fb15d47baef7477e616d18f
apple <apple@192.168.10.102>
2018-08-16 20:54:26 +0800

    merge info

--- TrainingModel/src/clustering/KModes.java
+++ TrainingModel/src/clustering/KModes.java
@@ -20,11 +20,31 @@ public class KModes {
 		//  if the changed data divide all data number less than 5%, the clustering process is over
 		//6 output all data group by cluster
 		
+		//testdata
+		//TODO
+		String[][] a = {{"book","red","james","58"},
+						{"vedio","blue","jane","68"},
+						{"book","blue","tom","58"},
+						{"vedio","green","jane","70"},
+						{"book","red","jane","48"}};
+		
+		
 		
 		//initial variables
-		int k = 5; //cluster number
+		//TODO
+		int k = 2; //cluster number
 		
 		List<List<String>> defectList = new ArrayList<List<String>>(); // init data set
+		//TODO
+		for(int i=0;i<a.length;i++) {
+			List<String> rList = new ArrayList<String>();
+			for(int j=0;j<a[i].length;j++) {
+				rList.add(a[i][j]);
+			}
+			defectList.add(rList);
+		}
+		
+		
 		
 		int defectsize = 0; //the count of all records
 		
@@ -37,33 +57,45 @@ public class KModes {
 		//begin
 		//get initial data set
 		Random rand = new Random();
-		QueryData qd = new QueryData();
-		defectList = qd.queryAll();
+//		QueryData qd = new QueryData();
+//		defectList = qd.queryAll();
 		defectsize = defectList.size();
 		recordSize = defectList.get(0).size(); //every defect's attributes count, equals 8
 		
 		//get k central point
-		
+		//TODO, randtemp must different,so need to add the limitation
 		for(int i=0; i < k; i++) {
 			int randTemp = rand.nextInt(defectsize);
-			defectList.get(randTemp).set(recordSize, i+"");  //set the last column is the type k
+			defectList.get(randTemp).add(4, i+"");  //set the last column is the type k
 			centralList.add(defectList.get(randTemp));
 		}
 		
 		//TODO
 		//clustering
 		for(int i=0; i < defectsize;i++) {
-			int count = 0;
 			for(int j=0; j < k; j++) {
-				for(int n=1; n < 9; n++) { // from n=1, because of defectID is different with each other
+				int count = 0;
+				//TODO N<8
+				for(int n=0; n < 4; n++) { // from n=1, because of defectID is different with each other
 					if(defectList.get(i).get(n).equals(centralList.get(j).get(n))) {
 						count++;
 					}
 				}
-				if(count > 3 && defectList.get(i).size() < 9) {
-					defectList.get(i).set(recordSize+1, centralList.get(j).get(recordSize+1));
+				//TODO count > 3 && defectList.get(i).size() < 9
+				if(count >= 2 && defectList.get(i).size() < 6) {
+					if(defectList.get(i).size() == 5) {
+						defectList.get(i).set(4, centralList.get(j).get(4));
+					}else {
+						defectList.get(i).add(4, centralList.get(j).get(4));
+					}
 				}
 			}
+		}
+		
+		//TODO
+		//first time, output defectList
+		for(int i=0; i<defectList.size();i++) {
+			System.out.println(defectList.get(i).toString());
 		}
 		
 		
@@ -72,16 +104,17 @@ public class KModes {
 		for(int n=0;n<k;n++) {
 			//each time find one cluster
 			for(int j=0; j<defectsize;j++ ) {
-				if(defectList.get(j).get(recordSize).equals(n+"")) {
+				if(defectList.get(j).size() == recordSize+1 && defectList.get(j).get(recordSize).equals(n+"")) {
 					kList.add(defectList.get(j));
 				}
 			}
 			//count the number of each record's attributes and record the frequent attribute as the central point attribute
-			HashMap<String,Integer> attributeMap = new HashMap<String,Integer>();
+			HashMap<String,Integer> attributeMap = null;
 			List<String> newCentralPointList = new ArrayList<String>();
-			
-			for(int i=1; i< recordSize+1;i++) {  //size is equals records size + 1(type column) , from i=1,because of the defectID is different with each other
+			//TODO
+			for(int i=0; i< recordSize+1;i++) {  //size is equals records size + 1(type column) , from i=1,because of the defectID is different with each other
 				for(int m=0; m<kList.size();m++) {   //iterator the one type records
+					attributeMap = new HashMap<String,Integer>();
 					String key = kList.get(m).get(i);
 					if(attributeMap.containsKey(key)) {
 						attributeMap.put(key,attributeMap.get(key)+1);
@@ -106,17 +139,18 @@ public class KModes {
 					String tempType = kList.get(t).get(kList.get(0).size()-1);
 					if(tempType.equals(clusterType)) {
 						//compare the two records match rate, if the rate is higher ,don't update the central point, else update the central point
+						
 					}
 				
 				
 				
 			}
-			
-			
 			kList = null;  //next cluster, clean list
 		}
 		//output the result
-		
+		for(Integer getkey:clusterMap.keySet()) {
+			
+		}
 		
 
 		
